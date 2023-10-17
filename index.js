db29cb0ebfe9f8e40a1770c723fc82e5c80420d9
Nutsa_Margvelashvili <nutsa_margvelashvili@epam.com>
2023-10-18 00:06:14 +0400

    add react

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
-const express = require("express")
+const express = require("express");
 const app = express();
-const http = require('http');
-const cors = require('cors');
-const {Server} = require("socket.io")
-app.use(cors())
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+app.use(cors());
 
-
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: "https://nutsamargvelashvili.github.io",
-        method: ["GET", "POST"]
-    }
-})
+  cors: {
+    origin: "https://nutsamargvelashvili.github.io",
+    method: ["GET", "POST"],
+  },
+});
 
-io.on("connection", (socket)=>{
-    console.log(`User Connected ${socket.id}`)
+io.on("connection", (socket) => {
+  console.log(`User Connected ${socket.id}`);
 
-    socket.on("join_room", (data)=>{
-        socket.join(data)
-        console.log(`User with ID: ${socket.id} joined room: ${data}`);
-    })
-    socket.on("send_message", (data) => {
-        console.log(data,'1')
-        socket.to(data.room).emit("receive_message", data)
-    })
-    socket.on("disconnect", ()=>{
-        console.log('User Disconnected', socket.id);
-    })
-})
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+  socket.on("send_message", (data) => {
+    console.log(data, "1");
+    socket.to(data.room).emit("receive_message", data);
+  });
+  socket.on("react_message", (data) => {
+    console.log(data, "react");
+    socket.to(data.room).emit("reacted_message", data);
+  });
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
 
-app.get('/hello', (req, res) => {
-    res.send('Hello, World!');
+app.get("/hello", (req, res) => {
+  res.send("Hello, World!");
 });
 
 server.listen(3001, () => {
-    console.log('SERVER RUNNING');
-})
+  console.log("SERVER RUNNING");
+});
